c9abd8039c4eaf30e9407cf1887e136407bd90a3
darmoul22 <yessine.darmoul@esprit.tn>
2023-10-24 10:25:40 +0100

    criterias and dates

--- src/appointment/appointment.controller.ts
+++ src/appointment/appointment.controller.ts
@@ -1,34 +1,77 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common'
-import { AppointmentService } from './appointment.service'
-import { CreateAppointmentDto } from './dto/create-appointment.dto'
-import { UpdateAppointmentDto } from './dto/update-appointment.dto'
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
+import { AppointmentService } from './appointment.service';
+import { CreateAppointmentDto } from './dto/create-appointment.dto';
+import { UpdateAppointmentDto } from './dto/update-appointment.dto';
 
-@Controller('appointments')
+@Controller('appointment')
 export class AppointmentController {
   constructor(private readonly appointmentService: AppointmentService) {}
 
   @Post()
   create(@Body() createAppointmentDto: CreateAppointmentDto) {
-    return this.appointmentService.create(createAppointmentDto)
+    return this.appointmentService.create(createAppointmentDto);
   }
 
-  @Get()
-  findAll() {
-    return this.appointmentService.findAll()
+  @Get('user/:userId')
+  findAllByUserId(
+    @Param('userId') userId: string,
+    @Query('startDate') startDate?: string,
+  ) {
+    const parsedStartDate = startDate ? new Date(startDate) : undefined;
+    return this.appointmentService.getAppointmentsByUserId(
+      +userId,
+      parsedStartDate,
+    );
+  }
+
+  @Get('service/:serviceId')
+  findAllByServiceId(
+    @Param('serviceId') serviceId: string,
+    @Query('startDate') startDate?: string,
+  ) {
+    const parsedStartDate = startDate ? new Date(startDate) : undefined;
+    return this.appointmentService.getAppointmentsByServiceId(
+      +serviceId,
+      parsedStartDate,
+    );
+  }
+
+  @Get('customer/:customerId')
+  findAllByCustomerId(
+    @Param('customerId') customerId: string,
+    @Query('startDate') startDate?: string,
+  ) {
+    const parsedStartDate = startDate ? new Date(startDate) : undefined;
+    return this.appointmentService.getAppointmentsByCustomerId(
+      +customerId,
+      parsedStartDate,
+    );
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.appointmentService.findOne(+id)
+    return this.appointmentService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAppointmentDto: UpdateAppointmentDto) {
-    return this.appointmentService.update(+id, updateAppointmentDto)
+  update(
+    @Param('id') id: string,
+    @Body() updateAppointmentDto: UpdateAppointmentDto,
+  ) {
+    return this.appointmentService.update(+id, updateAppointmentDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.appointmentService.remove(+id)
+    return this.appointmentService.remove(+id);
   }
 }
